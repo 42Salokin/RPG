e650d3d2baa5d717b9274d4ed5a2ac77a6a9c57e
Salokin <nicholas.dpoul@gmail.com>
2024-04-05 20:46:09 -0400

    add timers to space out events and on-screen dialogue describing events for hero side of combat

--- script.js
+++ script.js
@@ -28,26 +28,14 @@ function heroAttack() {
     let attempt = Math.floor(Math.random() * 20);
     attempt = (attempt + 1);
     console.log(`Hero attacks with ${attempt} vs creature's AC of ${mobAC}`);
+    mAnnounce.textContent = ""
     hAnnounce.textContent = "Hero attacks..."
     if (attempt >= mobAC && attempt < 20) {
         setTimeout(heroHits, 1500)
     } else if (attempt == 20) {
-        console.log("Critical hit!");
-        let damage = Math.floor(Math.random() * (heroDamage.length));
-        damage = ((damage + 1) * 2);
-        console.log(`And deals ${damage} damage`);
-        mobHP = (mobHP-damage)
-        console.log(`Creature is down to ${mobHP} HP`); 
-        mHP.textContent = mobHP       
-    } 
-    else {
-        console.log("It misses");
-    }
-    if (mobHP <= 0) {  
-       console.log(`Hero kills the creature!`);
-       setTimeout(mobRefresh, 3000)
+        setTimeout(heroCrit, 1500)       
     } else {
-        setTimeout(mobAttack, 3000)
+        setTimeout(heroMiss, 1500)
     }
 }
 
@@ -57,15 +45,59 @@ function heroHits() {
     setTimeout(heroDeals, 1000);
 }
 
+function heroCrit() {
+    console.log("Critical hit!");
+    hAnnounce.textContent = "Critical hit!";
+    setTimeout(heroCritDeals, 1000);
+}
+
+function heroMiss() {
+    console.log("It misses");
+    hAnnounce.textContent = "It misses"
+    setTimeout(mobAttack, 3000)
+}
+
 function heroDeals() {
     let damage = Math.floor(Math.random() * (heroDamage.length));
     damage = (damage + 1);
     console.log(`And deals ${damage} damage`);
-    hAnnounce.textContent = ''
+    hAnnounce.textContent = '';
     mAnnounce.textContent = `And deals ${damage} damage`;
     mobHP = (mobHP-damage);
-    console.log(`Creature is down to ${mobHP} HP`);
     mHP.textContent = mobHP;
+    if (mobHP <= 0) {  
+        setTimeout(mobDeath, 1500);
+     } else {
+        setTimeout(mobUpdate, 1500);
+     }
+}
+
+function heroCritDeals() {
+    let damage = Math.floor(Math.random() * (heroDamage.length));
+    damage = ((damage + 1) * 2);
+    console.log(`And deals ${damage} damage`);
+    hAnnounce.textContent = '';
+    mAnnounce.textContent = `And deals ${damage} damage`;
+    mobHP = (mobHP-damage)
+    mHP.textContent = mobHP
+    if (mobHP <= 0) {  
+        setTimeout(mobDeath, 1500);
+     } else {
+        setTimeout(mobUpdate, 1500);
+     }
+}
+
+function mobDeath() {
+    console.log(`Hero kills the creature!`);
+    mAnnounce.textContent = "";
+    hAnnounce.textContent = "Hero kills the creature!";
+    setTimeout(mobRefresh, 3000);
+}
+
+function mobUpdate() {
+    console.log(`Creature is down to ${mobHP} HP`);
+    mAnnounce.textContent = `Creature is down to ${mobHP} HP`
+    setTimeout(mobAttack, 3000)
 }
 
 function mobAttack() {
@@ -100,6 +132,8 @@ function mobAttack() {
 function mobRefresh() {
     mobHP = mob.HP
     console.log(`A new foe appears! Hero has ${heroHP} HP`);
+    mAnnounce.textContent = "";
+    hAnnounce.textContent = `A new foe appears! Hero has ${heroHP} HP`
     mHP.textContent = mobHP
 }
 // heroAttack()
