830c36262f8c732d4aaa89aa82114dea6c69e4e7
Salokin <nicholas.dpoul@gmail.com>
2024-04-26 19:27:26 -0400

    added second js file to hold mob stats and functions

--- mob.js
+++ mob.js
@@ -1,11 +1,72 @@
-const mob = {
-    HP: 8,
-    AC: 10,
-    Exp: 5,
-    Gold: 5,
+let i;
+let mob;
+let mobName;
+let mobHP;
+let mobAC;
+let mobExp;
+let mobGold;
+let mobDamage;
+
+function selectMob() {
+    let random = Math.random();
+    switch (heroLevel) {
+        case 1:
+            i = 0;
+            makeMob(i);
+            break;
+        case 2:
+            switch (true) {
+                case (random < 0.60):
+                    i = 0;
+                    makeMob(i);
+                    break;
+                default:
+                    i = 1;
+                    makeMob(i);
+            }
+            break;
+        default:
+            switch (true) {
+                case (random < 0.20):
+                    i = 0;
+                    makeMob(i);
+                    break;
+                case (random >= 0.20 && random < 0.60):
+                    i = 1;
+                    makeMob(i);
+                    break;
+                default:
+                    i = 2;
+                    makeMob(i);
+            }
+    }
 }
 
-module.exports = {
-    mob,
+
+function makeMob(i) {
+mob = new mobGenerator(mobs[i].Name, mobs[i].HP, mobs[i].AC, mobs[i].Exp, mobs[i].Gold, mobs[i].Damage)
+console.log(mob);
+mobName = mob.Name;
+mobHP = parseInt(mob.HP);
+mobAC = parseInt(mob.AC);
+mobExp = parseInt(mob.Exp);
+mobGold = parseInt(mob.Gold);
+mobDamage = parseInt(mob.Damage);
+console.log(mobHP, mobAC, mobExp, mobGold, mobDamage);
+mName.textContent = mobName;
+mHP.textContent = mobHP;
 }
 
+function mobGenerator(Name, HP, AC, Exp, Gold, Damage) {
+    this.Name = Name;
+    this.HP = HP;
+    this.AC = AC;
+    this.Exp = Exp;
+    this.Gold = Gold;
+    this.Damage = Damage;
+}
+
+const goblin = new mobGenerator('Goblin','8', '10', '5', '5', '6');
+const hobgoblin = new mobGenerator('Hobgoblin', '10', '12', '10', '10', '6' )
+const orc = new mobGenerator('Orc', '12', '12', '15', '15', '8')
+const mobs = [goblin, hobgoblin, orc];
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// const mobJS = require('./mob.js')
-// console.log(mobJS);
 const hHP = document.querySelector('#hHP');
 const hAC = document.querySelector('#hAC');
 const hLvl = document.querySelector('#hLvl');
@@ -28,6 +26,7 @@ const hero = {
     Dex: 1,
     Con: 1,
 }
+
 let heroMaxHP = hero.MaxHP;
 let heroHP = hero.HP;
 let heroLevel = hero.Level;
@@ -49,70 +48,6 @@ hStr.textContent = heroStr;
 hDex.textContent = heroDex;
 hCon.textContent = heroCon;
 let heroDamage = hero.Damage;
-
-function mobGenerator(Name, HP, AC, Exp, Gold, Damage) {
-    this.Name = Name;
-    this.HP = HP;
-    this.AC = AC;
-    this.Exp = Exp;
-    this.Gold = Gold;
-    this.Damage = Damage;
-}
-
-const goblin = new mobGenerator('Goblin','8', '10', '5', '5', '6');
-const hobgoblin = new mobGenerator('Hobgoblin', '10', '12', '10', '10', '6' )
-const orc = new mobGenerator('Orc', '12', '12', '15', '15', '8')
-const mobs = [goblin, hobgoblin, orc];
-
-let i;
-let mob;
-let mobName;
-let mobHP;
-let mobAC;
-let mobExp;
-let mobGold;
-let mobDamage;
-
-function selectMob() {
-    let random = Math.random();
-    if (heroLevel === 1) {
-        i = 0;
-        makeMob(i);
-    } else if (heroLevel === 2) {
-        if (random < 0.60) {
-            i = 0;
-            makeMob(i);
-        } else {
-            i = 1;
-            makeMob(i);
-        }
-    } else {
-        if (random < 0.20) {
-            i = 0;
-            makeMob(i);
-        } else if ((random >= 0.20) && (random < 0.60)) {
-            i = 1;
-            makeMob(i);
-        } else {
-            i = 2;
-            makeMob(i);            
-        }
-    }
-}
-
-function makeMob(i) {
-mob = new mobGenerator(mobs[i].Name, mobs[i].HP, mobs[i].AC, mobs[i].Exp, mobs[i].Gold, mobs[i].Damage)
-console.log(mob);
-mobName = mob.Name;
-mobHP = parseInt(mob.HP);
-mobAC = parseInt(mob.AC);
-mobExp = parseInt(mob.Exp);
-mobGold = parseInt(mob.Gold);
-mobDamage = parseInt(mob.Damage);
-console.log(mobHP, mobAC, mobExp, mobGold, mobDamage);
-mName.textContent = mobName;
-mHP.textContent = mobHP;
-}
 
 selectMob();
 
