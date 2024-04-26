fb766d737c934c0158a223b1aa8a537f4f66e093
Salokin <nicholas.dpoul@gmail.com>
2024-04-26 09:46:47 -0400

    added level abilities up to level 5, added 2 new creatures and included random selector function based on level

---
+++ mob.js
@@ -0,0 +1,11 @@
+const mob = {
+    HP: 8,
+    AC: 10,
+    Exp: 5,
+    Gold: 5,
+}
+
+module.exports = {
+    mob,
+}
+
--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const hHP = document.querySelector('#hHP')
-const hAC = document.querySelector('#hAC')
-const hLvl = document.querySelector('#hLvl')
-const hExp = document.querySelector('#hExp')
-const hGold = document.querySelector('#hGold')
-const hStr = document.querySelector('#hStr')
-const hDex = document.querySelector('#hDex')
-const hCon = document.querySelector('#hCon')
-const mHP = document.querySelector('#mHP')
-const hAnnounce = document.querySelector('#hAnnounce')
-const mAnnounce = document.querySelector('#mAnnounce')
-const levAnnounce = document.querySelector('#levAnnounce')
-const levList = document.querySelector('#levList')
-const statList = document.querySelector('#statList')
+// const mobJS = require('./mob.js')
+// console.log(mobJS);
+const hHP = document.querySelector('#hHP');
+const hAC = document.querySelector('#hAC');
+const hLvl = document.querySelector('#hLvl');
+const hExp = document.querySelector('#hExp');
+const hGold = document.querySelector('#hGold');
+const hStr = document.querySelector('#hStr');
+const hDex = document.querySelector('#hDex');
+const hCon = document.querySelector('#hCon');
+const mName = document.querySelector('#mName');
+const mHP = document.querySelector('#mHP');
+const hAnnounce = document.querySelector('#hAnnounce');
+const mAnnounce = document.querySelector('#mAnnounce');
+const levAnnounce = document.querySelector('#levAnnounce');
+const levList = document.querySelector('#levList');
+const statList = document.querySelector('#statList');
 
 const hero = {
     MaxHP: 10,
     HP: 10,
     AC: 12,
+    Damage: 8,
     Level: 1,
     Exp: 0,
     Gold: 10,
@@ -24,50 +28,101 @@ const hero = {
     Dex: 1,
     Con: 1,
 }
-let heroMaxHP = hero.MaxHP
-let heroHP = hero.HP
-let heroLevel = hero.Level
-let heroExp = hero.Exp
-let heroGold = hero.Gold
-let heroStr = hero.Str
-let atkBonus = parseInt(heroStr/2)
-let heroDex = hero.Dex
-let acBonus = parseInt(heroDex/2)
-let heroAC = (hero.AC + acBonus)
-let heroCon = hero.Con
-let hpBonus = parseInt(heroCon/2)
-hHP.textContent = heroHP
-hAC.textContent = heroAC
-hLvl.textContent = heroLevel
-hExp.textContent = heroExp
-hGold.textContent = heroGold
-hStr.textContent = heroStr
-hDex.textContent = heroDex
-hCon.textContent = heroCon
-let heroDamage = [1, 2, 3, 4, 5, 6, 7, 8]
-let levelHP = [1, 2, 3, 4, 5]
+let heroMaxHP = hero.MaxHP;
+let heroHP = hero.HP;
+let heroLevel = hero.Level;
+let heroExp = hero.Exp;
+let heroGold = hero.Gold;
+let heroStr = hero.Str;
+let atkBonus = parseInt(heroStr/2);
+let heroDex = hero.Dex;
+let acBonus = parseInt(heroDex/2);
+let heroAC = (hero.AC + acBonus);
+let heroCon = hero.Con;
+let hpBonus = parseInt(heroCon/2);
+hHP.textContent = heroHP;
+hAC.textContent = heroAC;
+hLvl.textContent = heroLevel;
+hExp.textContent = heroExp;
+hGold.textContent = heroGold;
+hStr.textContent = heroStr;
+hDex.textContent = heroDex;
+hCon.textContent = heroCon;
+let heroDamage = hero.Damage;
 
-const mob = {
-    HP: 8,
-    AC: 10,
-    Exp: 5,
-    Gold: 5,
+function mobGenerator(Name, HP, AC, Exp, Gold, Damage) {
+    this.Name = Name;
+    this.HP = HP;
+    this.AC = AC;
+    this.Exp = Exp;
+    this.Gold = Gold;
+    this.Damage = Damage;
 }
-let mobHP = mob.HP
-let mobAC = mob.AC
-let mobExp = mob.Exp
-let mobGold = mob.Gold
-mHP.textContent = mobHP
-let mobDamage = [1, 2, 3, 4, 5, 6]
 
+const goblin = new mobGenerator('Goblin','8', '10', '5', '5', '6');
+const hobgoblin = new mobGenerator('Hobgoblin', '10', '12', '10', '10', '6' )
+const orc = new mobGenerator('Orc', '12', '12', '15', '15', '8')
+const mobs = [goblin, hobgoblin, orc];
+
+let i;
+let mob;
+let mobName;
+let mobHP;
+let mobAC;
+let mobExp;
+let mobGold;
+let mobDamage;
+
+function selectMob() {
+    let random = Math.random();
+    if (heroLevel === 1) {
+        i = 0;
+        makeMob(i);
+    } else if (heroLevel === 2) {
+        if (random < 0.60) {
+            i = 0;
+            makeMob(i);
+        } else {
+            i = 1;
+            makeMob(i);
+        }
+    } else {
+        if (random < 0.20) {
+            i = 0;
+            makeMob(i);
+        } else if ((random >= 0.20) && (random < 0.60)) {
+            i = 1;
+            makeMob(i);
+        } else {
+            i = 2;
+            makeMob(i);            
+        }
+    }
+}
+
+function makeMob(i) {
+mob = new mobGenerator(mobs[i].Name, mobs[i].HP, mobs[i].AC, mobs[i].Exp, mobs[i].Gold, mobs[i].Damage)
+console.log(mob);
+mobName = mob.Name;
+mobHP = parseInt(mob.HP);
+mobAC = parseInt(mob.AC);
+mobExp = parseInt(mob.Exp);
+mobGold = parseInt(mob.Gold);
+mobDamage = parseInt(mob.Damage);
+console.log(mobHP, mobAC, mobExp, mobGold, mobDamage);
+mName.textContent = mobName;
+mHP.textContent = mobHP;
+}
+
+selectMob();
 
 function heroAttack() {
     let attempt = Math.floor(Math.random() * 20);
-    attempt = (attempt + atkBonus + 1);
-    console.log(`Hero attacks with ${attempt} vs creature's AC of ${mobAC}`);
+    attempt = 20 //(attempt + 1);
+    console.log(`Hero attacks with ${attempt} vs ${mobName}'s AC of ${mobAC}`);
     mAnnounce.textContent = ""
     hAnnounce.textContent = "Hero attacks..."
-    if (attempt >= mobAC && attempt < 20) {
+    if ((attempt + atkBonus) >= mobAC && attempt < 20) {
         setTimeout(heroHits, 1500)
     } else if (attempt == 20) {
         setTimeout(heroCrit, 1500)       
@@ -95,8 +150,8 @@ function heroMiss() {
 }
 
 function heroDeals() {
-    let damage = Math.floor(Math.random() * (heroDamage.length));
-    damage = (damage + atkBonus + 1);
+    let damage = ((Math.floor(Math.random() * heroDamage)) + atkBonus + 1);
+    // damage = (damage + atkBonus + 1);
     console.log(`And deals ${damage} damage`);
     hAnnounce.textContent = '';
     mAnnounce.textContent = `And deals ${damage} damage`;
@@ -110,8 +165,8 @@ function heroDeals() {
 }
 
 function heroCritDeals() {
-    let damage = Math.floor(Math.random() * (heroDamage.length));
-    damage = ((damage + 1) * 2);
+    let damage = (((Math.floor(Math.random() * heroDamage)) + atkBonus + 1) * 2);
+    // damage = ((damage + atkBonus + 1) * 2);
     console.log(`And deals ${damage} damage`);
     hAnnounce.textContent = '';
     mAnnounce.textContent = `And deals ${damage} damage`;
@@ -125,9 +180,9 @@ function heroCritDeals() {
 }
 
 function mobDeath() {
-    console.log(`Hero kills the creature!`);
+    console.log(`Hero kills the ${mobName}!`);
     mAnnounce.textContent = "";
-    hAnnounce.textContent = "Hero kills the creature!";
+    hAnnounce.textContent = `Hero kills the ${mobName}!`;
     setTimeout(heroReward, 2000);
 }
 
@@ -136,6 +191,12 @@ function heroReward() {
     hAnnounce.textContent = `Hero collects ${mobGold} gold and receives ${mobExp} exp`;
     heroExp = (heroExp + mobExp);
     if ((heroExp >= 15) && (heroLevel === 1)) {
+        setTimeout(levelUp, 500)
+    } else if ((heroExp >= 50) && (heroLevel === 2)) {
+        setTimeout(levelUp, 500)
+    } else if ((heroExp >= 200) && (heroLevel === 3)) {
+        setTimeout(levelUp, 500)
+    } else if ((heroExp >= 750) && (heroLevel === 4)) {
         setTimeout(levelUp, 500)
     }
     heroGold = (heroGold + mobGold);
@@ -166,25 +227,28 @@ function heroRun() {
 }
 
 function mobRefresh() {
-    mobHP = mob.HP
+    // mobName = mob.Name
+    // mobHP = mob.HP
+    selectMob();
     console.log(`A new foe appears! Hero has ${heroHP} HP`);
     hAnnounce.textContent = "";
     mAnnounce.textContent = `A new foe appears! Hero has ${heroHP} HP`
-    mHP.textContent = mobHP
+    // mName.textContent = mobName
+    // mHP.textContent = mobHP
 }
 
 function mobUpdate() {
-    console.log(`Creature is down to ${mobHP} HP`);
-    mAnnounce.textContent = `Creature is down to ${mobHP} HP`
+    console.log(`${mobName} is down to ${mobHP} HP`);
+    mAnnounce.textContent = `${mobName} is down to ${mobHP} HP`
     setTimeout(mobAttack, 3000)
 }
 
 function mobAttack() {
-    let attempt = Math.floor(Math.random() * 20);
-    attempt = (attempt + 1);
-    console.log(`Creature attacks with ${attempt} vs hero's AC of ${heroAC}`);
+    let attempt = ((Math.floor(Math.random() * 20)) + 1);
+    // attempt = (attempt + 1);
+    console.log(`${mobName} attacks with ${attempt} vs hero's AC of ${heroAC}`);
     hAnnounce.textContent = ""
-    mAnnounce.textContent = "Creature attacks..."
+    mAnnounce.textContent = `${mobName} attacks...` 
     if (attempt >= heroAC && attempt < 20) {
         setTimeout(mobHits, 1500)
     } else if (attempt == 20) {
@@ -212,8 +276,8 @@ function mobMiss() {
 }
 
 function mobDeals() {
-    let damage = Math.floor(Math.random() * (mobDamage.length));
-    damage = (damage + 1);
+    let damage = ((Math.floor(Math.random() * mobDamage)) + 1);
+    // damage = (damage + 1);
     console.log(`And deals ${damage} damage`);
     mAnnounce.textContent = '';
     hAnnounce.textContent = `And deals ${damage} damage`;
@@ -227,8 +291,8 @@ function mobDeals() {
 }
 
 function mobCritDeals() {
-    let damage = Math.floor(Math.random() * (mobDamage.length));
-    damage = ((damage + 1) * 2);
+    let damage = (((Math.floor(Math.random() * mobDamage)) + 1) * 2);
+    // damage = ((damage + 1) * 2);
     console.log(`And deals ${damage} damage`);
     mAnnounce.textContent = '';
     hAnnounce.textContent = `And deals ${damage} damage`;
@@ -254,16 +318,17 @@ function heroUpdate() {
 }
 
 function levelUp() {
+    empty(levList);
     // pop up message over screen announcing level up
     levAnnounce.style.visibility = "visible";
     // display new level
     heroLevel++;
     hLvl.textContent = heroLevel;
     // reset HP to max, add new HP, make new max
-    let newHP = Math.floor(Math.random() * (levelHP.length));
+    let newHP = Math.floor(Math.random() * 5);
     newHP = (newHP + hpBonus + 6);
     heroMaxHP = (heroMaxHP + newHP);
-    heroHP = heroMaxHP;
+    heroHP = (heroHP + newHP);
     // display new HP
     hHP.textContent = heroHP;
     console.log(`Level Up! Hero has reached Level ${heroLevel}!`);
@@ -272,13 +337,21 @@ function levelUp() {
     const levText1 = document.createElement("li");
     const levText2 = document.createElement("li");
     const levText3 = document.createElement("li");
+    
     levText1.setAttribute("class", "levText");
     levText2.setAttribute("class", "levText");
     levText3.setAttribute("class", "levText");
+    
     levText1.textContent = `Level Up!`;
     levText2.textContent = `Hero has reached Level ${heroLevel}!`;
     levText3.textContent = `Hero gains ${newHP} more HP`;
     levList.append(levText1, levText2, levText3);
+}
+
+function empty(element) {
+    while(element.firstElementChild) {
+        element.firstElementChild.remove();
+    }
 }
 
 statList.addEventListener('click', (event) => {
@@ -288,7 +361,6 @@ statList.addEventListener('click', (event) => {
 })
 
 function statUpgrade(selectedStat) {
-    console.log(selectedStat);
     switch (selectedStat) {
         case "Dex +1 Adds to Armor Class":
             heroDex = (heroDex + 1);
